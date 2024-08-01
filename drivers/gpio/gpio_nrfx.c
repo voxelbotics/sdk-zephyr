46b68c1f391439d0c3d7f1572bf131caf55402ea
Alexander Yurtsev <alex@emcraft.com>
2024-08-01 14:56:00 +0300

    drivers: gpio: gpio_nrfx: Update GPIO IRQ counters
    CB24FW-207: Update GPIO IRQ counters
    
    Signed-off-by: Alexander Yurtsev <alex@emcraft.com>
    Signed-off-by: Dmitry Starchenko <dmitry.starchenko@emcraft.com>
    (cherry picked from commit 3dac7fd1a87911d81d0633f951611bec69b4d83d)

--- drivers/gpio/gpio_nrfx.c
+++ drivers/gpio/gpio_nrfx.c
@@ -18,6 +18,7 @@ struct gpio_nrfx_data {
 	/* gpio_driver_data needs to be first */
 	struct gpio_driver_data common;
 	sys_slist_t callbacks;
+	uint32_t *counters;
 };
 
 struct gpio_nrfx_cfg {
@@ -26,6 +27,7 @@ struct gpio_nrfx_cfg {
 	NRF_GPIO_Type *port;
 	uint32_t edge_sense;
 	uint8_t port_num;
+	uint8_t ngpios;
 	nrfx_gpiote_t gpiote;
 };
 
@@ -379,7 +381,12 @@ static void nrfx_gpio_handler(nrfx_gpiote_pin_t abs_pin,
 	}
 
 	struct gpio_nrfx_data *data = get_port_data(port);
+	const struct gpio_nrfx_cfg *cfg = get_port_cfg(port);
 	sys_slist_t *list = &data->callbacks;
+
+	if (pin < cfg->ngpios) {
+		data->counters[pin]++;
+	}
 
 	gpio_fire_callbacks(list, port, BIT(pin));
 }
@@ -459,11 +466,16 @@ static const struct gpio_driver_api gpio_nrfx_drv_api_funcs = {
 		},							\
 		.port = _CONCAT(NRF_P, DT_INST_PROP(id, port)),		\
 		.port_num = DT_INST_PROP(id, port),			\
+		.ngpios = DT_INST_PROP_OR(id, ngpios, 32),		\
 		.edge_sense = DT_INST_PROP_OR(id, sense_edge_mask, 0),	\
 		.gpiote = GPIOTE_INSTANCE(id),				\
 	};								\
 									\
-	static struct gpio_nrfx_data gpio_nrfx_p##id##_data;		\
+	static uint32_t gpio_nrfx_p##id##_irq_counters[DT_INST_PROP_OR( \
+			id, ngpios, 32)];				\
+	static struct gpio_nrfx_data gpio_nrfx_p##id##_data = {		\
+		.counters = gpio_nrfx_p##id##_irq_counters		\
+	};								\
 									\
 	DEVICE_DT_INST_DEFINE(id, gpio_nrfx_init,			\
 			 NULL,						\
